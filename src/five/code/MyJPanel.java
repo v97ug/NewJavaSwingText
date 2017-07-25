cbb42ed52b986f585ccbed0dde8ad6bab2bf625d
V97ug <s1220164take@gmail.com>
2017-07-25 15:05:45 +0900

    [implement] 5 section

---
+++ src/five/code/MyJPanel.java
@@ -0,0 +1,45 @@
+package five.code;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class MyJPanel extends JPanel{
+    public void game(int width, int height) {
+        Image img = createImage(width, height);
+        Graphics g = img.getGraphics();
+        Graphics wg = getGraphics();
+
+        //追加！
+        int x = 200;
+        int y = 200;
+
+        while (true) {
+            g.setColor(Color.WHITE);
+            g.fillRect(0,0,width, height);
+
+            if(Key.up){
+                y--;
+            }
+            if(Key.down){
+                y++;
+            }
+            if(Key.right){
+                x++;
+            }
+            if(Key.left){
+                x--;
+            }
+
+            //追加！
+            g.setColor(Color.GREEN);
+            g.fillRect(x,y,50,50);
+
+            wg.drawImage(img, 0, 0, null);
+            try {
+                Thread.sleep(10);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
